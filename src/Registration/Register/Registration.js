e04199dd5452af4fce1e5c39fee1797087ebb008
Jaylano <jaylanovanderveen@gmail.com>
2025-09-11 21:08:04 +0200

    Update registration form with default values and change API endpoint

--- src/Registration/Register/Registration.js
+++ src/Registration/Register/Registration.js
@@ -7,10 +7,10 @@ const TandartsRegistratie = () => {
   const navigate = useNavigate();
 
   const [formData, setFormData] = useState({
-    name: "",
-    email: "",
-    password: "",
-    confirmPassword: "",
+    name: "testname",
+    email: "test@example.com",
+    password: "testPassword123",
+    confirmPassword: "testPassword123",
   });
 
   const [message, setMessage] = useState("");
@@ -35,7 +35,7 @@ const TandartsRegistratie = () => {
 
     try {
       const response = await fetch(
-        "http://localhost/tandartspraktijk/PHP/register.php",
+        "http://localhost/tandartspraktijkBackend/register/register.php",
         {
           method: "POST",
           headers: {
