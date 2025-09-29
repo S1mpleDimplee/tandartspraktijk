89256399a5b944f2c2729f1fc92bcd60a98f4deb
Jaylano <jaylanovanderveen@gmail.com>
2025-09-29 11:29:12 +0200

    Add option for '3' in role filters and improve address handling in DentistUser component

--- src/Dashboards/DashboardManager/UserInfo/UserInfo.js
+++ src/Dashboards/DashboardManager/UserInfo/UserInfo.js
@@ -104,6 +104,7 @@ const UserInfo = () => {
                 <option value="0">0</option>
                 <option value="1">1</option>
                 <option value="2">2</option>
+                <option value="3">3</option>
               </select>
             </div>
 
@@ -118,6 +119,7 @@ const UserInfo = () => {
                 <option value="0">0</option>
                 <option value="1">1</option>
                 <option value="2">2</option>
+                <option value="3">3</option>
               </select>
             </div>
 
