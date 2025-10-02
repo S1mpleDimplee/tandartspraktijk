059d7d377c0a154fc30f7e61f0178574f282ec7b
S1mpleDimplee <jaylanovanderveen@gmail.com>
2025-10-02 19:55:24 +0200

    Add option for role selection in UserPage component

--- src/Dashboards/DashboardManager/UserInfo/UserPage/UserPage.js
+++ src/Dashboards/DashboardManager/UserInfo/UserPage/UserPage.js
@@ -214,6 +214,7 @@ const UserPage = () => {
                   <option value={0}>0</option>
                   <option value={1}>1</option>
                   <option value={2}>2</option>
+                  <option value={3}>3</option>
                 </select>
               </div>
 
